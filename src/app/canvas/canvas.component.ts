dc9de75273f6666fed9d9e8bfa33d4a5164d6aaa
Rafael <rperez@grupofrisa.com>
2022-04-29 14:05:05 -0500

    Adicion de enemigos

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -43,9 +43,12 @@ class MainScene extends Phaser.Scene{
     { setting: 'music', value: true },
     { setting: 'sfx', value: true }
   ];
-
   cursors;
   stars;
+  score = 0;
+  scoreText;
+  bombs;
+
   constructor() {
     super({ key: 'main' });
   }
@@ -106,12 +109,19 @@ class MainScene extends Phaser.Scene{
       repeat: 11,
       setXY: { x: 12, y: 0, stepX: 70 }
     });
-  
     this.stars.children.iterate(function (child) {
       child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
     this.physics.add.collider(this.stars, this.platforms);
     this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
+    // Asignamos los datos en la vatiable scoretext 
+    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', color: '#000' });
+    // Añadimos las bombas
+    this.bombs = this.physics.add.group();
+
+    this.physics.add.collider(this.bombs, this.platforms);
+
+    this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
   }
 
   
@@ -140,5 +150,31 @@ class MainScene extends Phaser.Scene{
   // Funciones Extras, programar los eventos que se necesitan
   collectStar (player, star){
     star.disableBody(true, true);
+    //Actualizamos el 
+    this.score += 10;
+    this.scoreText.setText('Score: ' + this.score);
+    if (this.stars.countActive(true) === 0) {
+        this.stars.children.iterate(function (child) {
+          child.enableBody(true, child.x, 0, true, true);
+        });
+
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+        let bomb = this.bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+
+    }
+  }
+
+  hitBomb (player, bomb){
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    //gameOver = true;
   }
 }
